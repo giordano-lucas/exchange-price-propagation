37923b8d1a302a277d134c6b0dedf7507346b3d6
Augustin Kapps <kappsaugustin@gmail.com>
2022-01-19 14:13:15 +0100

    new set of data

--- helpers/preprocessing.py
+++ helpers/preprocessing.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 
 def convert_time(df):
     df["date"] = pd.to_datetime(
@@ -8,5 +8,10 @@ def convert_time(df):
 
 
 def transform_to_returns(x):
+    ret = np.log(x).diff(1).dropna()
+    return ret
+
+
+def transform_to_bin_ret(x):
     ret = x.diff(1).dropna() > 0
     return ret*1-1*(1-ret)
