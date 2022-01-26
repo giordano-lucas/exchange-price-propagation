3e235dfc38f62281b9c5d6a975d1016aeeedf748
Lucas Giordano <giordano3102lucas@gmail.com>
2022-01-26 15:10:30 +0100

    add figures to gh pages push script

---
+++ scripts/figures_to_gh_pages.sh
@@ -0,0 +1,9 @@
+git pull
+git checkout gh-pages
+git pull
+git pull
+git checkout main -- figures
+git add figures
+git commit -m "Update 'figures' directory from 'main' branch"
+git push
+git checkout main
